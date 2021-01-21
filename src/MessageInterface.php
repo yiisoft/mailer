c5ec7958218e498147f43d7cec055fc7a0115278
devanych <mail@devanych.ru>
2021-01-21 18:39:33 +0300

    Make MessageInterface is immutable

--- src/MessageInterface.php
+++ src/MessageInterface.php
@@ -9,125 +9,140 @@ use Throwable;
 /**
  * MessageInterface is the interface that should be implemented by mail message classes.
  *
- * A message represents the settings and content of an email, such as the sender, recipient,
- * subject, body, etc.
+ * A message represents the settings and content of an email, such as the sender, recipient, subject, body, etc.
  *
- * Messages are sent by a [[\yii\mail\MailerInterface]], like the following,
+ * Messages are sent by a {@see \Yiisoft\Mailer\MailerInterface}, like the following:
  *
  * ```php
  * $mailer->compose()
- *     ->setFrom('from@domain.com')
- *     ->setTo($form->email)
- *     ->setSubject($form->subject)
- *     ->setTextBody('Plain text content')
- *     ->setHtmlBody('<b>HTML content</b>')
+ *     ->withFrom('from@domain.com')
+ *     ->withTo($form->email)
+ *     ->withSubject($form->subject)
+ *     ->withTextBody('Plain text content')
+ *     ->withHtmlBody('<b>HTML content</b>')
  *     ->send();
  * ```
- *
- * @see MailerInterface
  */
 interface MessageInterface
 {
     /**
-     * Sets mailer.
+     * Returns a new instance with the specified mailer instance.
      *
-     * @param MailerInterface $mailer
+     * @param MailerInterface $mailer The mailer instance.
      *
-     * @return $this self reference.
+     * This method MUST be implemented in such a way as to retain the immutability of the message,
+     * and MUST return an instance that has the new mailer instance.
+     *
+     * @return self
      */
-    public function setMailer(MailerInterface $mailer): self;
+    public function withMailer(MailerInterface $mailer): self;
 
     /**
-     * Returns the character set of this message.
+     * Returns the charset of this message.
      *
-     * @return string the character set of this message.
+     * @return string The charset of this message.
      */
     public function getCharset(): string;
 
     /**
-     * Sets the character set of this message.
+     * Returns a new instance with the specified charset.
      *
-     * @param string $charset character set name.
+     * This method MUST be implemented in such a way as to retain the immutability of the message,
+     * and MUST return an instance that has the new charset.
      *
-     * @return $this self reference.
+     * @param string $charset The charset name.
+     *
+     * @return self
      */
-    public function setCharset(string $charset): self;
+    public function withCharset(string $charset): self;
 
     /**
-     * Returns the message sender.
+     * Returns the message sender email address.
      *
-     * @return array|string the sender
+     * @return array|string The sender email address.
      */
     public function getFrom();
 
     /**
-     * Sets the message sender.
+     * Returns a new instance with the specified sender email address.
      *
-     * @param array|string $from sender email address.
+     * @param array|string $from The sender email address.
+     *
      * You may pass an array of addresses if this message is from multiple people.
-     * You may also specify sender name in addition to email address using format:
-     * `[email => name]`.
+     * You may also specify sender name in addition to email address using format: `[email => name]`.
      *
-     * @return $this self reference.
+     * This method MUST be implemented in such a way as to retain the immutability of the message,
+     * and MUST return an instance that has the new sender email address.
+     *
+     * @return self
      */
-    public function setFrom($from): self;
+    public function withFrom($from): self;
 
     /**
-     * Returns the message recipient(s).
+     * Returns the message recipient(s) email address.
      *
-     * @return array|string the message recipients
+     * @return array|string The message recipients email address.
      */
     public function getTo();
 
     /**
-     * Sets the message recipient(s).
+     * Returns a new instance with the specified recipient(s) email address.
      *
      * @param array|string $to receiver email address.
-     * You may pass an array of addresses if multiple recipients should receive this message.
-     * You may also specify receiver name in addition to email address using format:
-     * `[email => name]`.
      *
-     * @return $this self reference.
+     * You may pass an array of addresses if multiple recipients should receive this message.
+     * You may also specify receiver name in addition to email address using format: `[email => name]`.
+     *
+     * This method MUST be implemented in such a way as to retain the immutability of the message,
+     * and MUST return an instance that has the new recipients email address.
+     *
+     * @return self
      */
-    public function setTo($to): self;
+    public function withTo($to): self;
 
     /**
      * Returns the reply-to address of this message.
      *
-     * @return array|string the reply-to address of this message.
+     * @return array|string The reply-to address of this message.
      */
     public function getReplyTo();
 
     /**
-     * Sets the reply-to address of this message.
+     * Returns a new instance with the specified reply-to address.
      *
-     * @param array|string $replyTo the reply-to address.
+     * @param array|string $replyTo The reply-to address.
+     *
      * You may pass an array of addresses if this message should be replied to multiple people.
-     * You may also specify reply-to name in addition to email address using format:
-     * `[email => name]`.
+     * You may also specify reply-to name in addition to email address using format: `[email => name]`.
      *
-     * @return $this self reference.
+     * This method MUST be implemented in such a way as to retain the immutability of the message,
+     * and MUST return an instance that has the new reply-to address.
+     *
+     * @return self
      */
-    public function setReplyTo($replyTo): self;
+    public function withReplyTo($replyTo): self;
 
     /**
      * Returns the Cc (additional copy receiver) addresses of this message.
      *
-     * @return array|string the Cc (additional copy receiver) addresses of this message.
+     * @return array|string The Cc (additional copy receiver) addresses of this message.
      */
     public function getCc();
 
     /**
-     * Sets the Cc (additional copy receiver) addresses of this message.
+     * Returns a new instance with the specified Cc (additional copy receiver) addresses.
      *
-     * @param array|string $cc copy receiver email address.
+     * @param array|string $cc The copy receiver email address.
+     *
      * You may pass an array of addresses if multiple recipients should receive this message.
-     * You may also specify receiver name in addition to email address using format:
-     * `[email => name]`.
+     * You may also specify receiver name in addition to email address using format: `[email => name]`.
      *
-     * @return $this self reference.
+     * This method MUST be implemented in such a way as to retain the immutability of the message,
+     * and MUST return an instance that has the new Cc (additional copy receiver) addresses.
+     *
+     * @return self
      */
-    public function setCc($cc): self;
+    public function withCc($cc): self;
 
     /**
      * Returns the Bcc (hidden copy receiver) addresses of this message.
@@ -137,171 +152,193 @@ interface MessageInterface
     public function getBcc();
 
     /**
-     * Sets the Bcc (hidden copy receiver) addresses of this message.
+     * Returns a new instance with the specified Bcc (hidden copy receiver) addresses.
      *
-     * @param array|string $bcc hidden copy receiver email address.
+     * @param array|string $bcc The hidden copy receiver email address.
+     *
      * You may pass an array of addresses if multiple recipients should receive this message.
-     * You may also specify receiver name in addition to email address using format:
-     * `[email => name]`.
+     * You may also specify receiver name in addition to email address using format: `[email => name]`.
      *
-     * @return $this self reference.
+     * This method MUST be implemented in such a way as to retain the immutability of the message,
+     * and MUST return an instance that has the new Bcc (hidden copy receiver) addresses.
+     *
+     * @return self
      */
-    public function setBcc($bcc): self;
+    public function withBcc($bcc): self;
 
     /**
      * Returns the message subject.
      *
-     * @return string the message subject
+     * @return string The message subject.
      */
     public function getSubject(): string;
 
     /**
-     * Sets the message subject.
+     * Returns a new instance with the specified message subject.
      *
-     * @param string $subject message subject
+     * This method MUST be implemented in such a way as to retain the immutability of the message,
+     * and MUST return an instance that has the new message subject.
      *
-     * @return $this self reference.
+     * @param string $subject The message subject.
+     *
+     * @return self
      */
-    public function setSubject(string $subject): self;
+    public function withSubject(string $subject): self;
 
     /**
      * Returns the message text body.
      *
-     * @return string the message text body.
+     * @return string The message text body.
      */
     public function getTextBody(): string;
 
     /**
-     * Sets message plain text content.
+     * Returns a new instance with the specified message plain text content.
      *
-     * @param string $text message plain text content.
+     * This method MUST be implemented in such a way as to retain the immutability of the message,
+     * and MUST return an instance that has the new message plain text content.
      *
-     * @return $this self reference.
+     * @param string $text The message plain text content.
+     *
+     * @return self
      */
-    public function setTextBody(string $text): self;
+    public function withTextBody(string $text): self;
 
     /**
      * Returns the message HTML body.
      *
-     * @return string the message HTML body.
+     * @return string The message HTML body.
      */
     public function getHtmlBody(): string;
 
     /**
-     * Sets message HTML content.
+     * Returns a new instance with the specified message HTML content.
+     *
+     * This method MUST be implemented in such a way as to retain the immutability of the message,
+     * and MUST return an instance that has the new message HTML content.
      *
      * @param string $html message HTML content.
      *
-     * @return $this self reference.
+     * @return self
      */
-    public function setHtmlBody(string $html): self;
+    public function withHtmlBody(string $html): self;
 
     /**
-     * Attaches existing file to the email message.
+     * Returns a new instance with the specified attached existing file.
      *
-     * @param string $fileName full file name
-     * @param array $options options for embed file. Valid options are:
+     * @param string $fileName The full file name.
+     * @param array $options The options for attached file. Valid options are:
      *
      * - fileName: name, which should be used to attach file.
      * - contentType: attached file MIME type.
      *
-     * @return $this self reference.
+     * This method MUST be implemented in such a way as to retain the immutability of the message,
+     * and MUST return an instance that has the new attached existing file.
+     *
+     * @return self
      */
-    public function attach(string $fileName, array $options = []): self;
+    public function withAttached(string $fileName, array $options = []): self;
 
     /**
-     * Attach specified content as file for the email message.
+     * Returns a new instance with the specified attached content as file.
      *
-     * @param string $content attachment file content.
-     * @param array $options options for embed file. Valid options are:
+     * @param string $content The content of the attached file.
+     * @param array $options The options for attached file. Valid options are:
      *
      * - fileName: name, which should be used to attach file.
      * - contentType: attached file MIME type.
      *
-     * @return $this self reference.
+     * This method MUST be implemented in such a way as to retain the immutability of the message,
+     * and MUST return an instance that has the new attached content as file.
+     *
+     * @return self
      */
-    public function attachContent(string $content, array $options = []): self;
+    public function withAttachedContent(string $content, array $options = []): self;
 
     /**
      * Attach a file and return it's CID source.
+     *
      * This method should be used when embedding images or other data in a message.
      *
-     * @param string $fileName file name.
-     * @param array $options options for embed file. Valid options are:
+     * @param string $fileName The full file name.
+     * @param array $options The options for embed file. Valid options are:
      *
      * - fileName: name, which should be used to attach file.
      * - contentType: attached file MIME type.
      *
-     * @return string attachment CID.
+     * This method MUST be implemented in such a way as to retain the immutability of the message,
+     * and MUST create an instance that has the new attached existing file.
+     *
+     * @return string The attachment CID.
      */
     public function embed(string $fileName, array $options = []): string;
 
     /**
      * Attach a content as file and return it's CID source.
+     *
      * This method should be used when embedding images or other data in a message.
      *
-     * @param string $content attachment file content.
-     * @param array $options options for embed file. Valid options are:
+     * @param string $content The attachment file content.
+     * @param array $options The options for embed file. Valid options are:
      *
      * - fileName: name, which should be used to attach file.
      * - contentType: attached file MIME type.
      *
-     * @return string attachment CID.
+     * This method MUST be implemented in such a way as to retain the immutability of the message,
+     * and MUST create an instance that has the new attached content as file.
+     *
+     * @return string The attachment CID.
      */
     public function embedContent(string $content, array $options = []): string;
 
     /**
-     * Adds custom header value to the message.
-     * Several invocations of this method with the same name will add multiple header values.
-     *
-     * @param string $name header name.
-     * @param string $value header value.
-     *
-     * @return $this self reference.
-     */
-    public function addHeader(string $name, string $value): self;
-
-    /**
-     * Sets custom header value to the message.
-     *
-     * @param string $name header name.
-     * @param array|string $value header value or values.
-     *
-     * @return $this self reference.
-     */
-    public function setHeader(string $name, $value): self;
-
-    /**
      * Returns all values for the specified header.
      *
-     * @param string $name header name.
+     * @param string $name The header name.
      *
-     * @return array header values list.
+     * @return array The header values list.
      */
     public function getHeader(string $name): array;
 
     /**
-     * Sets custom header values to the message.
+     * Returns a new instance with the specified added custom header value.
      *
-     * @param array $headers headers in format: `[name => value]`.
+     * Several invocations of this method with the same name will add multiple header values.
      *
-     * @return $this self reference.
+     * @param string $name The header name.
+     * @param string $value The header value.
+     *
+     * This method MUST be implemented in such a way as to retain the immutability of the message,
+     * and MUST return an instance that has the new added custom header value.
+     *
+     * @return self
      */
-    public function setHeaders(array $headers): self;
+    public function withAddedHeader(string $name, string $value): self;
 
     /**
-     * Sends this email message.
+     * Returns a new instance with the specified custom header value.
      *
-     * @throws Throwable throws an exception on send fails.
+     * @param string $name The header name.
+     * @param array|string $value The header value or values.
+     *
+     * This method MUST be implemented in such a way as to retain the immutability of the message,
+     * and MUST return an instance that has the new custom header value.
+     *
+     * @return self
      */
-    public function send(): void;
+    public function withHeader(string $name, $value): self;
 
     /**
-     * Returns string representation of this message.
+     * Returns a new instance with the specified custom header values.
      *
-     * @return string the string representation of this message.
+     * @param array $headers The headers in format: `[name => value]`.
+     *
+     * This method MUST be implemented in such a way as to retain the immutability of the message,
+     * and MUST return an instance that has the new custom header values.
+     *
+     * @return self
      */
-    public function toString(): string;
+    public function withHeaders(array $headers): self;
 
     /**
      * Returns error represents why send fails.
@@ -311,9 +348,28 @@ interface MessageInterface
     public function getError(): Throwable;
 
     /**
-     * Sets send fails error.
+     * Returns a new instance with the specified send fails error.
      *
-     * @param Throwable $e
+     * @param Throwable $e The send fails error.
+     *
+     * This method MUST be implemented in such a way as to retain the immutability of the message,
+     * and MUST return an instance that has the new send fails error.
+     *
+     * @return self
      */
-    public function setError(Throwable $e): void;
+    public function withError(Throwable $e): self;
+
+    /**
+     * Sends this email message.
+     *
+     * @throws Throwable If the send fails.
+     */
+    public function send(): void;
+
+    /**
+     * Returns string representation of this message.
+     *
+     * @return string The string representation of this message.
+     */
+    public function __toString(): string;
 }
