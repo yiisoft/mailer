0a0fa4b5877b5145d12d975367ca3cea21b80678
Sergei Predvoditelev <sergei@predvoditelev.ru>
2024-10-09 15:20:45 +0300

    `NullMailer` tests

--- src/NullMailer.php
+++ src/NullMailer.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 namespace Yiisoft\Mailer;
 
+/**
+ * This mailer can be used to avoid send messages.
+ */
 final class NullMailer implements MailerInterface
 {
     public function send(MessageInterface $message): void
